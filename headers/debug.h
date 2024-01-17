a57cf538f29ef57c1ccd6f5459b981c4f7c0dd17
Sebastian Schoeneich <Sebastian.Schoeneich@gmx.de>
2024-01-17 09:17:57 +0100

    Added semicolons

--- headers/debug.h
+++ headers/debug.h
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #define DEBUG_FILE "/../_DEBUG/DEBUG_output.txt"
 
-char *calc_time = ""
-char *disp_time = ""
-char packages = ""
-char customers = ""
-char worker_status = ""
+char *calc_time = "";
+char *disp_time = "";
+char packages = "";
+char customers = "";
+char worker_status = "";
 
 char *calc_time 
 int debug_output(calc_time, disp_time, packages, customers, worker_status) {
@@ -14,42 +14,42 @@ int debug_output(calc_time, disp_time, packages, customers, worker_status) {
     FILE *f = fopen(DEBUG_FILE, "w");
     if (f == NULL) {
         printf("ERROR: File %s could not be opened", filename);
-        return -1
+        return -1;
     }
 
     // write to file
     if (calc_time != "") {
         d_ct = calc_time;
     } else {
-        d_ct = "No internal calculation time found."
+        d_ct = "No internal calculation time found.";
     }
-    fprintf(f, "Internal Calculation Time: %s\n", d_ct)
+    fprintf(f, "Internal Calculation Time: %s\n", d_ct);
 
     if (disp_time != "") {
         d_dt = disp_time;
     } else {
-        d_dt = "No display time found."
+        d_dt = "No display time found.";
     }
-    fprintf(f, "Display Time: %s\n", d_dt)
+    fprintf(f, "Display Time: %s\n", d_dt);
     
     if (packages != "") {
         d_pk = packages;
     } else {
-        d_pk = "No packages found."
+        d_pk = "No packages found.";
     }
-    fprintf(f, "Packages: %s\n", d_dt)
+    fprintf(f, "Packages: %s\n", d_dt);
 
     if (customers != "") {
         d_cs = customers;
     } else {
-        d_cs = "No customers found."
+        d_cs = "No customers found.";
     }
-    fprintf(f, "Customers: %s\n", d_dt)
+    fprintf(f, "Customers: %s\n", d_dt);
 
     if (worker_status != "") {
         d_cs = worker_status;
     } else {
-        d_cs = "No worker found."
+        d_cs = "No worker found.";
     }
-    fprintf(f, "Worker: %s\n", d_dt)
+    fprintf(f, "Worker: %s\n", d_dt);
 }
