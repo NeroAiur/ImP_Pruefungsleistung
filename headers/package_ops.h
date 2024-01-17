fdf58c5e98e244e070b4f73fd2dfc37f6a26d49b
Sebastian Schoeneich <Sebastian.Schoeneich@gmx.de>
2024-01-17 11:02:51 +0100

    Started working on package operations

--- headers/package_ops.h
+++ headers/package_ops.h
@@ -1,1 +1,57 @@
 /*Code for package operations*/
+#include <stdio.h>
+
+int package_size = 0;
+int station_status;
+
+int input_package(int package_size, int station_status) {
+    if (0 <= package_size >= 6) {
+        printf("ERROR: package_size is not in range 1-5. package_size: %s", package_size);
+    }
+
+    // we need to decide how the station status will stored
+    // until that I'll just leave this free how to get the data and will use one possible integration
+
+    int free_xs = 0;
+    for (int i = 0; i < station_status.xs; i++) {
+        if (station_status.xs == 0) {
+            free_s += 1;
+        }
+    }
+
+    int free_s = 0;
+    for (int i = 0; i < station_status.s; i++) {
+        if (station_status.s == 0) {
+            free_s += 1;
+        }
+    }
+
+    int free_s = 0;
+    for (int i = 0; i < station_status.s; i++) {
+        if (station_status.s == 0) {
+            free_s += 1;
+        }
+    }
+    
+    int free_m = 0;
+    for (int i = 0; i < station_status.m; i++) {
+        if (station_status.m == 0) {
+            free_m += 1;
+        }
+    }
+    
+    int free_l = 0;
+    for (int i = 0; i < station_status.l; i++) {
+        if (station_status.l == 0) {
+            free_l += 1;
+        }
+    }
+    
+    int free_xl = 0;
+    for (int i = 0; i < station_status.xl; i++) {
+        if (station_status.xl == 0) {
+            free_xl += 1;
+        }
+    }
+    
+}
---
+++ todo.md
@@ -0,0 +1,12 @@
+# TODO-List
+
+## Priority 1
+* package station status
+> * how to store it?
+> * what needs to be stored?
+
+## Priority 2
+
+## Priority 3
+
+## Backlog
