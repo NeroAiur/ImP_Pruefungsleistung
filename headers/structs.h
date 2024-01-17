6fb6d971d489b1e36715bbf3790c5ea52026dde7
Paul Weibbrecht <paulweibbrecht@gmail.com>
2024-01-17 17:05:40 +0100

    Structs header + include guards

---
+++ headers/structs.h
@@ -0,0 +1,56 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+
+struct package {
+	
+	int package_id;
+	int package_size;
+	int package_sender_id;
+	int package_recipient_id;
+	
+};
+
+struct locker {
+	
+	int locker_id;
+	int locker_size;
+	struct package locker_content;
+	int locker_age;
+	
+};
+
+struct customer {
+	
+	int customer_id;
+	struct package packages_in_possession[15];
+	
+};
+
+struct postOfficeBox{
+	
+	int postOfficeBox_id;
+	
+	int isInUse;
+	
+	struct locker XS_locker[4];
+	struct locker S_locker[5];
+	struct locker M_locker[21];
+	struct locker L_locker[10];
+	struct locker XL_locker[2];
+	
+};
+
+struct Node  {
+	
+	struct package data;
+	struct Node* next;
+	struct Node* prev;
+	
+};
+
+struct listAdress {
+	struct Node* headAdress;
+};
+
+#endif
--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include ".\headers\DVL.h" /*functions for doubly linked list*/
 #include ".\headers\worker.h" /*functions for the worker of the post*/
+#include ".\headers\structs.h"
 
 #define screenCharX 40
 #define screenCharY 30
@@ -27,45 +28,6 @@ int Initialize();
 
 struct listAdress transactionQueue; /* global variable - pointer to head node.*/
 
-struct package {
-	
-	int package_id;
-	int package_size;
-	int package_sender_id;
-	int package_recipient_id;
-	
-};
-
-struct locker {
-	
-	int locker_id;
-	int locker_size;
-	struct package locker_content;
-	int locker_age;
-	
-};
-
-struct customer {
-	
-	int customer_id;
-	struct package packages_in_possession[15];
-	
-};
-
-struct postOfficeBox{
-	
-	int postOfficeBox_id;
-	
-	int isInUse;
-	
-	struct locker XS_locker[4];
-	struct locker S_locker[5];
-	struct locker M_locker[21];
-	struct locker L_locker[10];
-	struct locker XL_locker[2];
-	
-};
-
 char pseudoGrafix[screenCharX][screenCharY];
 
 struct customer customer_list[250];
@@ -77,8 +39,6 @@ int main(int argc, char *argv[]) {
 	
 	Initialize();
 	
-	drawScreen();
-	
 	while(TRUE){
 		
 		calculateTimeStep();
@@ -86,14 +46,19 @@ int main(int argc, char *argv[]) {
 		Sleep(500);
 		
 		if(kbhit()){
+			
 			InputChar= toupper(getch());
+			
 			switch (InputChar){
+				
 				case 'T': return 1;
 				case 'P': return 2;
 				case 'A': return 0;
+				
 			}
 				
 		}
+		
 		printf("%d",InputChar);
 		printf("%d",iteration);
 		iteration++;
@@ -159,7 +124,7 @@ void drawScreen(){
 
 
 int calculateTimeStep(){
-	printf("Calc");
+	
 	return 0;
 }
 
