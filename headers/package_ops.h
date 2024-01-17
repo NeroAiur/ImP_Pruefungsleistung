366571ab7ca4e5863cc93849112831652a58e083
Sebastian Schoeneich <business.sschoeneich@gmail.com>
2024-01-17 13:02:37 +0100

    Increased code efficiency (because it bothered me)

--- headers/package_ops.h
+++ headers/package_ops.h
@@ -2,14 +2,18 @@
 #include <stdio.h>
 
 int input_package(int package_size, int station_status) {
+
+    // Checks if there is an error regarding the opening of the file
     if (0 <= package_size >= 6) {
         printf("ERROR: package_size is not in range 1-5. package_size: %s", package_size);
     }
 
+    // Checks if there are free slots for every size of pocket
     int free_xs = 0;
     for (int i = 0; i < station_status.xs; i++) {
         if (station_status.xs == 0) {
             free_s += 1;
+            break;
         }
     }
 
@@ -17,6 +21,7 @@ int input_package(int package_size, int station_status) {
     for (int i = 0; i < station_status.s; i++) {
         if (station_status.s == 0) {
             free_s += 1;
+            break;
         }
     }
 
@@ -24,6 +29,7 @@ int input_package(int package_size, int station_status) {
     for (int i = 0; i < station_status.s; i++) {
         if (station_status.s == 0) {
             free_s += 1;
+            break;
         }
     }
     
@@ -31,6 +37,7 @@ int input_package(int package_size, int station_status) {
     for (int i = 0; i < station_status.m; i++) {
         if (station_status.m == 0) {
             free_m += 1;
+            break;
         }
     }
     
@@ -45,6 +52,7 @@ int input_package(int package_size, int station_status) {
     for (int i = 0; i < station_status.xl; i++) {
         if (station_status.xl == 0) {
             free_xl += 1;
+            break;
         }
     }
     
@@ -55,27 +63,27 @@ int input_package(int package_size, int station_status) {
     int use_slot_size;
     switch (package_size) {
         case 1:
-            if (free_xs > 0) {
+            if (free_xs != 0) {
                 use_slot_size = 1;
             }
 
         case 2:
-            if (free_s > 0) {
+            if (free_s != 0) {
                 use_slot_size = 2;
             }
 
         case 3:
-            if (free_m > 0) {
+            if (free_m != 0) {
                 use_slot_size = 3;
             }
         
         case 4:
-            if (free_l > 0) {
+            if (free_l != 0) {
                 use_slot_size = 4;
             }
 
         case 5:
-            if (free_xl > 0) {
+            if (free_xl != 0) {
                 use_slot_size = 5;
             } else {
                 use_slot_size = 10;
