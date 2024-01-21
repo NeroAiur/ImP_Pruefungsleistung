502343cf99d06275dd1888c6476c37e382f830e6
Sebastian Schoeneich <business.sschoeneich@gmail.com>
2024-01-21 11:27:55 +0100

    started working on console ouput module

--- headers/output.h
+++ headers/output.h
@@ -1,9 +1,24 @@
 /*Code for the output*/
 
+#include <stdio.h>
+
+#define OUTPUT_FILE ".\.\_DEBUG\output.txt"
+
 int refresh_output() {
 
 }
 
 int output_to_console() {
-    
+    FILE *file;
+
+    file = fopen(OUTPUT_FILE, "r");
+    char fileContent[10000];
+
+    fgets(fileContent, 10000, file);
+
+    // stuff after file is read
+    // file should be read line to line
+    // then output
+
+    fclose(file);
 }
