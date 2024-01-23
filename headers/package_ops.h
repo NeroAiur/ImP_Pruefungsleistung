7fcd4cb5b1e3c7d0369bf21ba52645d9ae517361
Sebastian Schoeneich <business.sschoeneich@gmail.com>
2024-01-23 14:07:55 +0100

    adjusted to struct structure

--- headers/package_ops.h
+++ headers/package_ops.h
@@ -1,58 +1,58 @@
 /*Code for package operations*/
 #include <stdio.h>
+#include ".\structs.h"
 
-int input_package(int package_size, int station_status) {
-    // station_status is a struct
-    // station_status.<SIZE> are arrays. each array has either 0 or 1 on their positions. 1 means "occupied" 0 means "free"
+#define xs_locker_amount 4
+#define s_locker_amount 5
+#define m_locker_amount 21
+#define l_locker_amount 10
+#define xl_locker_amount 2
+
+struct postOfficeBox input_package(struct package package, struct postOfficeBox postOfficeBox) {
+    int free_xs = 0;
+    int free_s = 0;
+    int free_m = 0;
+    int free_l = 0;
+    int free_xl = 0;
+
+    // postOfficeBox is a struct
+    // postOfficeBox.<SIZE> are arrays. each array has either 0 or 1 on their positions. 1 means "occupied" 0 means "free"
 
     // Checks if there is an error regarding the opening of the file
-    if (0 <= package_size >= 6) {
-        printf("ERROR: package_size is not in range 1-5. package_size: %s", package_size);
+    if (0 <= package.package_size >= 6) {
+        printf("ERROR: package.package_size is not in range 1-5. package.package_size: %s", package.package_size);
     }
 
     // Checks if there are free slots for each slot size
-    int free_xs = 0;
-    for (int i = 0; i < station_status.xs; i++) {
-        if (station_status.xs[i] == 0) {
-            free_s += 1;
+    for (int i = 0; i < xs_locker_amount; i++) { // 4 is the amount of XS-lockers at the station
+        if (postOfficeBox.XS_locker[i].isEmpty == 1) {
+            free_xs += 1;
             break;
         }
     }
 
-    int free_s = 0;
-    for (int i = 0; i < station_status.s; i++) {
-        if (station_status.s[i] == 0) {
-            free_s += 1;
-            break;
-        }
-    }
-
-    int free_s = 0;
-    for (int i = 0; i < station_status.s; i++) {
-        if (station_status.s[i] == 0) {
+    for (int i = 0; i < s_locker_amount; i++) { // 5 is the amount of S-lockers at the station
+        if (postOfficeBox.S_locker[i].isEmpty == 1) {
             free_s += 1;
             break;
         }
     }
     
-    int free_m = 0;
-    for (int i = 0; i < station_status.m; i++) {
-        if (station_status.m[i] == 0) {
+    for (int i = 0; i < m_locker_amount; i++) { // 21 is the amount of M-lockers at the station
+        if (postOfficeBox.M_locker[i].isEmpty == 1) {
             free_m += 1;
             break;
         }
     }
     
-    int free_l = 0;
-    for (int i = 0; i < station_status.l; i++) {
-        if (station_status.l[i] == 0) {
+    for (int i = 0; i < l_locker_amount; i++) {
+        if (postOfficeBox.L_locker[i].isEmpty == 1) {
             free_l += 1;
         }
     }
     
-    int free_xl = 0;
-    for (int i = 0; i < station_status.xl; i++) {
-        if (station_status.xl[i] == 0) {
+    for (int i = 0; i < xl_locker_amount; i++) {
+        if (postOfficeBox.XL_locker[i].isEmpty == 1) {
             free_xl += 1;
             break;
         }
@@ -60,16 +60,17 @@ int input_package(int package_size, int station_status) {
     
     // Packages can only be put into slots of their coresponding size, or larger.
     // The following switch-statement will start at the packages coresponding size and check if the sizes "free" value is true ("1").
-    // If thats the case, it will go through the station_status.<SIZE> array and change the first unoccupied slot (the first "0") and occupy it (change the value to "1").
+    // If thats the case, it will go through the postOfficeBox.<SIZE> array and change the first unoccupied slot (the first "0") and occupy it (change the value to "1").
     // If there is no unoccupied slot in that size, it will look for the next larger size, repeating until an unoccupied slot.
-    // This means there could be a package of size xs (package_size = 1) that will be put into a slot of size xl (most fitting for package_size 5).
+    // This means there could be a package of size xs (package.package_size = 1) that will be put into a slot of size xl (most fitting for package.package_size 5).
     // If there no occupiable slot, the package will be stored by the worker or taken home by a customer.
-    switch (package_size) {
+    switch (package.package_size) {
         case 1:
             if (free_xs != 0) {
-                for (int i = 0, i < station_status.xs; i++) {
-                    if (station_status.xs[i] == 0) {
-                        station_status.xs[i] = 1;
+                for (int i = 0; i < xs_locker_amount; i++) {
+                    if (postOfficeBox.XS_locker[i].isEmpty == 1) {
+                        postOfficeBox.XS_locker[i].locker_content = package;
+                        postOfficeBox.XS_locker[i].isEmpty = 0;
                         break;
                     }
                 }
@@ -77,9 +78,10 @@ int input_package(int package_size, int station_status) {
 
         case 2:
             if (free_s != 0) {
-                for (int i = 0, i < station_status.s; i++) {
-                    if (station_status.s[i] == 0) {
-                        station_status.s[i] = 1;
+                for (int i = 0; i < s_locker_amount; i++) {
+                    if (postOfficeBox.S_locker[i].isEmpty == 1) {
+                        postOfficeBox.S_locker[i].locker_content = package;
+                        postOfficeBox.S_locker[i].isEmpty = 0;
                         break;
                     }
                 }
@@ -87,9 +89,10 @@ int input_package(int package_size, int station_status) {
 
         case 3:
             if (free_m != 0) {
-                for (int i = 0, i < station_status.m; i++) {
-                    if (station_status.m[i] == 0) {
-                        station_status.m[i] = 1;
+                for (int i = 0; i < m_locker_amount; i++) {
+                    if (postOfficeBox.M_locker[i].isEmpty == 1) {
+                        postOfficeBox.M_locker[i].locker_content = package;
+                        postOfficeBox.M_locker[i].isEmpty = 0;
                         break;
                     }
                 }
@@ -97,9 +100,10 @@ int input_package(int package_size, int station_status) {
         
         case 4:
             if (free_l != 0) {
-                for (int i = 0, i < station_status.l; i++) {
-                    if (station_status.l[i] == 0) {
-                        station_status.l[i] = 1;
+                for (int i = 0; i < l_locker_amount; i++) {
+                    if (postOfficeBox.L_locker[i].isEmpty == 1) {
+                        postOfficeBox.L_locker[i].locker_content = package;
+                        postOfficeBox.L_locker[i].isEmpty = 0;
                         break;
                     }
                 }
@@ -107,19 +111,20 @@ int input_package(int package_size, int station_status) {
 
         case 5:
             if (free_xl != 0) {
-                for (int i = 0, i < station_status.xl; i++) {
-                    if (station_status.xl[i] == 0) {
-                        station_status.xl[i] = 1;
+                for (int i = 0; i < xl_locker_amount; i++) {
+                    if (postOfficeBox.XL_locker[i].isEmpty == 1) {
+                        postOfficeBox.XL_locker[i].locker_content = package;
+                        postOfficeBox.XL_locker[i].isEmpty = 0;
                         break;
                     }
                 }
             } else {
-                // ***********************************
-                // CODE MISSING FOR THE CASE WHERE THERE IS NO OCCUPIABLE SLOT
-                // ***********************************
+                /* *********************************** */
+                /* CODE MISSING FOR THE CASE WHERE THERE IS NO OCCUPIABLE SLOT */
+                /* *********************************** */
                 break;
             }
     }
 
-    return station_status;
+    return postOfficeBox;
 }
